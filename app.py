b1dd5a41a927228adb095c5ada6ca1ef302bc82e
carlos-moreno <omorenodovale@gmail.com>
2024-02-25 02:36:20 +0000

    feat: Comment operation in code

--- app.py
+++ app.py
@@ -3,45 +3,45 @@ import numpy as np
 from datetime import datetime
 
 
-def valida_numero_tipo_inteiro(df, column):
-    mask = df[column].notnull()
-    df.loc[mask, column] = pd.to_numeric(df.loc[mask, column], errors="coerce", downcast="unsigned").fillna("ERROR => '" + df.loc[mask, column].astype(str) + "' Motivo => Valor é inválido para o tipo 'Inteiro'")
+# def valida_numero_tipo_inteiro(df, column):
+#     mask = df[column].notnull()
+#     df.loc[mask, column] = pd.to_numeric(df.loc[mask, column], errors="coerce", downcast="unsigned").fillna("ERROR => '" + df.loc[mask, column].astype(str) + "' Motivo => Valor é inválido para o tipo 'Inteiro'")
 
-def valida_tamanho_string(df, column, length):
-    mask = df[column].notnull()
-    df.loc[mask, column] = df[column].where(df[column].str.len().between(0, length), "ERROR => '" + df[column] + "' Motivo => Excede o tamanho máximo de " + str(length) + " caracteres permitido para o campo")
+# def valida_tamanho_string(df, column, length):
+#     mask = df[column].notnull()
+#     df.loc[mask, column] = df[column].where(df[column].str.len().between(0, length), "ERROR => '" + df[column] + "' Motivo => Excede o tamanho máximo de " + str(length) + " caracteres permitido para o campo")
 
 nome_arquivo = 'dados.csv'
 df = pd.read_csv(nome_arquivo, sep=";", names=['A', 'B', 'C', 'D'], skipinitialspace = True)
 
-valida_numero_tipo_inteiro(df, 'B')
-valida_tamanho_string(df, 'A', 10)
+# valida_numero_tipo_inteiro(df, 'B')
+# valida_tamanho_string(df, 'A', 10)
 
-filtro = df.applymap(lambda x: str(x).startswith("ERROR => "))
+# filtro = df.applymap(lambda x: str(x).startswith("ERROR => "))
 
-df_invalido = df[filtro.any(axis=1)]
-df = df[~filtro.any(axis=1)]
+# df_invalido = df[filtro.any(axis=1)]
+# df = df[~filtro.any(axis=1)]
 
-tuplas_erros = []
-for _, row in df_invalido.iterrows():
-    erros = []
-    for column in df.columns:
-        if str(row[column]).startswith("ERROR => "):
-            erros.append(f"Coluna {column}: {row[column]}")
-    if erros:
-        tuplas_erros.append(
-            (
-                "Nome da tabela",
-                "Nome do arquivo",
-                " | ".join(erros),
-                datetime.now().isoformat(),
-            )
-        )
+# tuplas_erros = []
+# for _, row in df_invalido.iterrows():
+#     erros = []
+#     for column in df.columns:
+#         if str(row[column]).startswith("ERROR => "):
+#             erros.append(f"Coluna {column}: {row[column]}")
+#     if erros:
+#         tuplas_erros.append(
+#             (
+#                 "Nome da tabela",
+#                 "Nome do arquivo",
+#                 " | ".join(erros),
+#                 datetime.now().isoformat(),
+#             )
+#         )
 
 
 print(df)
-print(df_invalido)
-print(tuplas_erros)
+# print(df_invalido)
+# print(tuplas_erros)
 
 
 # df = df.where(pd.notnull(df), None)
