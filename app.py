4478eac14857deeeb196772cdc84f445ba176973
carlos-moreno <omorenodovale@gmail.com>
2024-03-04 19:25:43 +0000

    feat: Testing new approaches

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ import numpy as np
 from datetime import datetime
 
 
+def preencher_vazios(elemento):
+    if elemento == '':
+        return 'campo nao pode ser vazio'
+    else:
+        return elemento
+
 nome_arquivo = 'dados.csv'
-df = pd.read_csv(nome_arquivo, sep=";", names=['A', 'B', 'C', 'D'], skipinitialspace = True)
+df = pd.read_csv(nome_arquivo, sep=";", names=['A', 'B', 'C', 'D'], skipinitialspace = True, dtype=str, na_filter=False)
+
+# for coluna in colunas:
+#     df[coluna] = df[coluna].map(preencher_vazios)
+df[['A', 'D']] = df[['A', 'D']].where(df[['A', 'D']] != '', 'campo nao pode ser vazio')
+df['B'] = df['B'].where(df['B'] == '', pd.to_numeric(df['B'], errors='coerce', downcast='unsigned').fillna(df['B'] + ' Valor Inválido'))
+
+
 
 print(df)
