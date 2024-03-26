0ff333b7cf8ca966e3271c6c1fae9bf06715ef3a
Carlos Moreno <omorenodovale@gmail.com>
2024-03-25 23:04:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def preencher_vazios(elemento):
 nome_arquivo = 'dados.csv'
 df = pd.read_csv(nome_arquivo, sep=";", names=['A', 'B', 'C', 'D'], dtype=str, na_filter=False)
 
-df[['A', 'D']] = df[['A', 'D']].where(df[['A', 'D']] != '', 'campo nao pode ser vazio')
+df[['A', 'D']] = df[['A', 'D']].where(df[['A', 'D']] != '', 'Campo não pode ser vazio')
 df['B'] = df['B'].where(df['B'] == '', pd.to_numeric(df['B'], errors='coerce', downcast='unsigned').fillna(df['B'] + ' Valor Inválido'))
 
 
